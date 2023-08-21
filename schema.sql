3ed966603a2590e94ea6ca5f8cb666c480ce8334
manzitresor <83731517+manzitresor@users.noreply.github.com>
2023-08-21 15:26:44 +0200

    Add index for owners table

--- schema.sql
+++ schema.sql
@@ -55,3 +55,4 @@ CREATE TABLE specializations (
 ALTER TABLE owners ADD COLUMN email VARCHAR(120);
 
 CREATE INDEX visits_animal_id_idx ON visits (animal_id);
+CREATE INDEX owners_email_idx ON owners(email);
